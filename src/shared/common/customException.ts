82244613e75ff9e5fcd46638bab7dfc183e1f710
daniel-judit <daniel.resende@judit.io>
2024-11-22 12:23:54 -0300

    fix: common types

--- src/shared/common/customException.ts
+++ src/shared/common/customException.ts
@@ -2,8 +2,18 @@ import { HttpException } from '@nestjs/common';
 import { Logger } from '@nestjs/common';
 
 export class CustomException extends HttpException {
-    constructor(message: string, statusCode: number) {
-        super(message, statusCode);
-        new Logger().warn(`[${statusCode}] -` + message)
-    }
+  constructor(
+    message: string,
+    statusCode: number,
+    description = 'INVALID_DATA',
+  ) {
+    super(
+      {
+        error_code: description,
+        error_description: message,
+      },
+      statusCode,
+    );
+    new Logger().warn(`[${statusCode}] ` + message);
+  }
 }
---
+++ src/shared/common/validationPipe.ts
@@ -0,0 +1,27 @@
+import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import { validate } from 'class-validator';
+import { plainToClass } from 'class-transformer';
+import { CustomException } from './customException';
+
+@Injectable()
+export class CustomValidationPipe implements PipeTransform {
+  async transform(value: string, { metatype }: ArgumentMetadata) {
+    if (!metatype || !this.toValidate(metatype)) {
+      return value;
+    }
+    const object = plainToClass(metatype, value);
+    const errors = await validate(object);
+    if (errors.length > 0) {
+      const errorMessages = errors
+        .map((err) => Object.values(err.constraints).join(', '))
+        .join('; ');
+      throw new CustomException(`${errorMessages}`, 400);
+    }
+    return value;
+  }
+
+  private toValidate(metatype: Function): boolean {
+    const types: Function[] = [String, Boolean, Number, Array, Object];
+    return !types.includes(metatype);
+  }
+}
