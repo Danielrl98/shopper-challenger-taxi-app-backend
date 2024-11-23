1595a41a7fa46e91e9bb506896bc43ed6f149be0
daniel-judit <daniel.resende@judit.io>
2024-11-23 13:54:26 -0300

    feat: enable cors

--- src/main/index.ts
+++ src/main/index.ts
@@ -4,6 +4,14 @@ import { CustomValidationPipe } from '../shared/common/validationPipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization,Origin',
+    credentials: true,
+  });
+
   app.useGlobalPipes(new CustomValidationPipe());
   await app.listen(3000);
 }
